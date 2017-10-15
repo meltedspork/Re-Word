62af5f0b9a2e9508e73a39b836419bec8cf2c409
Jason Salerno <jason@meltedspork.com>
2017-10-15 11:12:36 -0500

    Add jsdoc [WIP]

---
+++ client-side/src/client-test-doc.js
@@ -0,0 +1,15 @@
+/**
+ * this is ClientTestDoc.
+ */
+class ClientTestDoc {
+  /**
+   * @param {number} a - this is a value.
+   * @param {number} b - this is a value.
+   * @return {number} result of the sum value.
+   */
+  sum(a, b){
+    return a + b;
+  }
+}
+
+export default ClientTestDoc;
--- server-side/src/schema/index.js
+++ server-side/src/schema/index.js
@@ -1,5 +1,11 @@
-const {makeExecutableSchema} = require('graphql-tools')
-const resolvers = require('./resolvers')
+/**
+ * @desc Build and mock your GraphQL.js schema using the schema language.
+ * @version 2.4.0
+ * @external graphql-tools
+ * @see {@link https://github.com/apollographql/graphql-tools graphql-tools}
+ */
+import { makeExecutableSchema } from 'graphql-tools'
+import resolvers from './resolvers'
 
 // Define your types here.
 const typeDefs = `
